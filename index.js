2f78d3929dceb20c04c021de844fcd12aef7fa31
fujiwaramisaki <105963169+fujiwaramisaki@users.noreply.github.com>
2022-05-22 14:04:41 +0900

    Update index.js

--- index.js
+++ index.js
@@ -36,21 +36,4 @@ client.on("messageCreate", (message)=> {
   }
 });
 
-function getQuote() {
-  return fetch("https://zenquotes.io/api/random")
-    .then(res => {
-      return res.json()
-      })
-    .then(data => {
-      return data[0]["q"] + " -" + data[0]["a"]
-    })
-}
-client.on("message", msg => {
-  if (msg.author.bot) return
-    
-  if (msg.content === "inspire") {
-    getQuote().then(quote => msg.channel.send(quote))
-  }
-})
-
 client.login("process.env.TOKEN");
